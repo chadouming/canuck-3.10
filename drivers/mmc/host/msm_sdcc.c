b51351566b20b8382c83757e3f4c104f737f5668
Devin Kim <dojip.kim@lge.com>
2013-12-18 11:46:42 -0600

    mmc: msm_sdcc: Support for WIFI_CONTROL_FUNC

--- drivers/mmc/host/msm_sdcc.c
+++ drivers/mmc/host/msm_sdcc.c
@@ -79,6 +79,12 @@
 #define MSM_MMC_BUS_VOTING_DELAY	200 /* msecs */
 #define INVALID_TUNING_PHASE		-1
 
+#if defined(CONFIG_WIFI_CONTROL_FUNC)
+extern int wcf_status_register(
+		void (*cb)(int card_present, void *dev), void *dev);
+extern unsigned int wcf_status(struct device *dev);
+#endif
+
 #if defined(CONFIG_DEBUG_FS)
 static void msmsdcc_dbg_createhost(struct msmsdcc_host *);
 static struct dentry *debugfs_dir;
@@ -5788,6 +5794,8 @@ static struct mmc_platform_data *msmsdcc_populate_pdata(struct device *dev)
 		pdata->nonremovable = true;
 	if (of_get_property(np, "qcom,disable-cmd23", NULL))
 		pdata->disable_cmd23 = true;
+	if (of_get_property(np, "qcom,wifi-control-func", NULL))
+		pdata->wifi_control_func = true;
 	of_property_read_u32(np, "qcom,dat1-mpm-int",
 					&pdata->mpm_sdiowakeup_int);
 
